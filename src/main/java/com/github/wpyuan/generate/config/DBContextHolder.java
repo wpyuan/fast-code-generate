e36478da1c5a25dd8123e9b4411ba47bbb5f2e72
wangpeiyuan <490176245@qq.com>
2021-02-01 16:51:31 +0800

    精简

--- src/main/java/com/github/wpyuan/generate/config/DBContextHolder.java
+++ src/main/java/com/github/wpyuan/generate/config/DBContextHolder.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class DBContextHolder {
     private static final ThreadLocal<String> contextHolder = new ThreadLocal<>();
 
-//    private static final AtomicInteger counter = new AtomicInteger(-1);
-
     public static void set(String dbType) {
         contextHolder.set(dbType);
     }
@@ -22,17 +20,7 @@ public class DBContextHolder {
     }
 
     public static void oracle() {
-        //  轮询
-//        int index = counter.getAndIncrement() % 2;
-//        if (counter.get() > 9999) {
-//            counter.set(-1);
-//        }
-//        if (index == 0) {
-            set("oracle");
-            log.info("切换到oracle");
-//        }else {
-//            set(DBTypeEnum.SLAVE2);
-//            log.info("切换到slave2");
-//        }
+        set("oracle");
+        log.info("切换到oracle");
     }
 }
